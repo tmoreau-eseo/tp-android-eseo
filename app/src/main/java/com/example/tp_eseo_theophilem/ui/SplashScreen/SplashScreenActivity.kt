7eece45a7c97eff98ea9fd491fa92d3c9bcf15ff
tmo <theophile.moreau@reseau.eseo.fr>
2021-04-14 17:30:17 +0200

    version 1

---
+++ app/src/main/java/com/example/tp_eseo_theophilem/ui/SplashScreen/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tp_eseo_theophilem.ui.SplashScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tp_eseo_theophilem.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+    }
+}
