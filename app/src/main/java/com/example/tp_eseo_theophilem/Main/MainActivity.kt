f43fdbbb085d0e75e9fc65159a0a3f7bf5e6b146
tmo <theophile.moreau@reseau.eseo.fr>
2021-04-15 14:42:45 +0200

    initial dev commit

--- app/src/main/java/com/example/tp_eseo_theophilem/Main/MainActivity.kt
+++ app/src/main/java/com/example/tp_eseo_theophilem/Main/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.tp_eseo_theophilem.Main
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tp_eseo_theophilem.R
@@ -9,4 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    companion object {
+        fun getStartIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
+
 }
--- app/src/main/java/com/example/tp_eseo_theophilem/ui/SplashScreen/SplashScreenActivity.kt
+++ app/src/main/java/com/example/tp_eseo_theophilem/ui/SplashScreen/SplashScreenActivity.kt
@@ -1,12 +1,24 @@
 package com.example.tp_eseo_theophilem.ui.SplashScreen
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tp_eseo_theophilem.Main.MainActivity
 import com.example.tp_eseo_theophilem.R
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+
+
+
+        //Lancement de la Main Activity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(MainActivity.getStartIntent(this))
+            finish()
+        }, 2000)
     }
 }
